2a51476d415dc620edcdaec88973be49e5824418
josue22almo <josue.almo@gmail.com>
2025-02-06 18:17:04 +0100

    chore: hoover transition working mas o menos

--- src/components/ui/character.card.tsx
+++ src/components/ui/character.card.tsx
@@ -13,22 +13,49 @@ export const CharacterCard = ({ character }: CharacterCardProps) => {
 
   return (
     <Card className="relative group overflow-hidden rounded-lg w-[172.5px] shadow-none border-none">
-      <CardContent className="aspect-square overflow-hidden p-0 border-b-[4px] border-[#ED1C24]">
+      <CardContent className="aspect-square overflow-hidden p-0">
         <img 
           src={character.image} 
           alt={character.name}
           className="w-full h-full"
         />
       </CardContent>
-      <CardFooter className="bg-black text-white p-2 flex justify-between items-center">
-        <span className="font-bold truncate">{character.name}</span>
-        <Button onClick={() => toggleFavorite(character)}>
-          <HeartIcon isFavorited={isFavorite(character)} />
-        </Button>
-        <div className="absolute right-0 bottom-0 w-[20px] h-[20px] overflow-hidden">
-          <div className="absolute bottom-0 right-0 w-0 h-0 border-l-[20px] border-l-black border-b-[20px] border-b-white"></div>
+
+      <CardFooter 
+        className="relative bg-black group-hover:bg-[#ED1C24] text-white p-2 flex justify-between items-center 
+                   transition-colors duration-300"
+      >
+        <HoverTab />
+        <div className="flex flex-row justify-between items-center w-full">
+          <span className="font-bold truncate">{character.name}</span>
+          <Button 
+            onClick={() => toggleFavorite(character)} 
+            className="p-1 transition-colors duration-300"
+          >
+            <HeartIcon 
+              isFavorited={isFavorite(character)} 
+              className={`w-5 h-5 transition-colors duration-300 ${
+                isFavorite(character) 
+                  ? "text-[#ED1C24]"
+                  : "text-white group-hover:text-black"
+              }`}
+            />
+          </Button>
+          {/* Triángulo decorativo */}
+          <div className="absolute right-0 bottom-0 w-[20px] h-[20px] overflow-hidden">
+            <div className="absolute bottom-0 right-0 w-0 h-0 border-l-[20px] group-hover:border-l-[#ED1C24] border-l-black border-b-[20px] group-hover:border-b-white border-b-white transition-colors duration-300"></div>
+          </div>
         </div>
       </CardFooter>
     </Card>
-  )
+  );
+};
+
+export const HoverTab: React.FC = () => {
+  return (
+    <div 
+        className="absolute top-0 left-0 w-full bg-[#ED1C24] 
+                   h-[5px] group-hover:h-[40px] transition-all duration-300 ease-in-out"
+      />
+  );
 };
--- src/components/ui/heart.icon.tsx
+++ src/components/ui/heart.icon.tsx
@@ -1,13 +1,15 @@
 import heartIcon from '../../assets/heart-icon.svg'
 import heartIconWhite from '../../assets/heart-icon-white.svg'
+import { cn } from '@/lib/utils';
 interface HeartIconProps {
   isFavorited: boolean;
   color?: string;
+  className?: string;
 }
 
-export const HeartIcon = ({ isFavorited }: HeartIconProps) => { 
+export const HeartIcon = ({ isFavorited, className }: HeartIconProps) => { 
   return (
-    <div className="flex flex-row items-center justify-center gap-2">
+    <div className={cn("flex flex-row items-center justify-center gap-2", className)}>
       <img src={isFavorited ? heartIcon : heartIconWhite} alt="Favorites Icon" style={{ width: '24px', height: '24px' }} />
     </div>
   );
