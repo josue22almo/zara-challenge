a2de95ab048e3af3f2bd031de8c047ea3a707658
josue22almo <josue.almo@gmail.com>
2025-02-06 16:27:51 +0100

    chore: top loading bar and static header

--- src/screens/home.screen.tsx
+++ src/screens/home.screen.tsx
@@ -1,32 +1,65 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import LoadingBar, { LoadingBarRef } from "react-top-loading-bar";
+
+
 import { Screen } from "@/screens/screen";
 import { CharacterCard } from "@/components/ui/character.card";
-import { useCharacters } from "@/contexts/character/hooks/useCharacters";
+import { useCharacters, useCharactersCount } from "@/contexts/character/hooks/useCharacters";
 import { useMarvelCharacterApi } from "@/contexts/character/insfractructure/marvel-character.api";
 import { Input } from "@/components/ui/input";
+
 export const HomeScreen = () => {
   const [search, setSearch] = useState(""); 
+  const ref = useRef<LoadingBarRef>(null);
+  
   const api = useMarvelCharacterApi(
     import.meta.env.VITE_MARVEL_API_KEY!,
     import.meta.env.VITE_MARVEL_PRIVATE_KEY!
   );
+
   const { data: characters, isLoading, error } = useCharacters(api, search);
+  const { data: charactersCount } = useCharactersCount(api, search);
+
+
+  useEffect(() => {
+    ref.current?.start();
+  }, [search]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      ref.current?.complete();
+    }
+  }, [isLoading]);
   
-  return <Screen>
-    <div className="flex flex-col gap-4"> 
-      <Input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-    </div>
-      {
-        characters?.map((character) => (
-          <CharacterCard key={character.id} character={character} />
-        ))
-      }
+  return (
+    <Screen>
+      <LoadingBar 
+        ref={ref}
+        shadow={false}
+        height={4}
+        style={{ position: 'absolute', top: 68, left: 0, right: 0, zIndex: 50 }}
+      />
+      <div className="flex flex-col gap-4 pt-12">
+        <div className="flex flex-col gap-4"> 
+          <Input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div>
+        <div className="flex flex-col gap-4"> 
+          <div className="text-sm text-black">
+            {charactersCount} RESULTS
+          </div>
+        </div>
+        {
+          characters?.map((character) => (
+            <CharacterCard key={character.id} character={character} />
+          ))
+        }
+        {
+          !characters?.length && !isLoading && <div>No characters found</div>
+        }
+      </div>
       {
         error && <div>Error: {error.message}</div>
       }
-      {
-        isLoading && <div>Loading...</div>
-      }
     </Screen>
-  ;
+  );
 };
--- src/screens/screen.tsx
+++ src/screens/screen.tsx
@@ -6,8 +6,10 @@ interface ScreenProps {
 
 export const Screen = ({ children }: ScreenProps) => {
   return <div className="flex flex-col h-screen">
-    <Header />
-    <div className="flex-1 px-4 sm:px-6 md:px-8 lg:px-12">
+    <div className="fixed top-0 left-0 right-0 z-50">
+      <Header />
+    </div>
+    <div className="flex-1 px-4 sm:px-6 md:px-8 lg:px-12 mt-16">
       {children}
     </div>
   </div>;
