7d71fa2a423ac1735289b5b34ec21b9d189db3a9
josue22almo <josue.almo@gmail.com>
2025-02-08 05:42:43 +0100

    chore: using dragon bal logo in the header

--- src/components/ui/app.icon.tsx
+++ src/components/ui/app.icon.tsx
@@ -1,8 +1,10 @@
+import { useCharacterApiContext } from '@/use-character.api.context';
 import marvelLogo from '/marvel-logo.svg'
-
+import dragonBallLogo from '/dragon-ball-logo.svg'
 export const AppIcon = () => {
+  const { mode } = useCharacterApiContext();
   return (
-    <img src={marvelLogo} alt="Marvel Logo" style={{ width: '130px', height: '52px' }} />
+    <img src={mode === 'marvel' ? marvelLogo : dragonBallLogo} alt="Marvel Logo" style={{ width: '130px', height: '52px' }} />
   );
 };
 
